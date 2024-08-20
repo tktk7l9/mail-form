5a06d8291722d88a9f3c11921fb26b005346f455
tktk7l9 <saitotakuya0719@gmail.com>
2024-08-20 13:10:51 +0900

    feat: セクション2：【フォーム開発実践】メール送信機能付きのお問い合わせフォームを作ろう

---
+++ app/api/send/route.ts
@@ -0,0 +1,39 @@
+import { EmailTemplate } from "../../../components/email-template";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+
+  const username = formData.get("username") as string;
+  const email = formData.get("email") as string;
+  const subject = formData.get("subject") as string;
+  const content = formData.get("content") as string;
+  const file = formData.get("file") as File;
+
+  const buffer = Buffer.from(await file.arrayBuffer());
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["saitotakuya0719@gmail.com"],
+      subject,
+      react: EmailTemplate({
+        username,
+        email,
+        content,
+      }) as React.ReactElement,
+      attachments: [{ filename: file.name, content: buffer }],
+    });
+
+    if (error) {
+      return NextResponse.json({ error });
+    }
+
+    return NextResponse.json({ data });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
